a0f492634ef93d0eb59b80f23eee84b97d262676
rjtprasad <prasadrajat28@gmail.com>
2024-05-23 20:56:31 +0530

    TMDB API Setup - Axios

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Movix is movies database application with top notch frontend technologies such as React, Redux & CSS.
+Movix is movies database application with top notch frontend technologies such as React, Redux & CSS.
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
+import {fetchDataFromApi} from "./utils/api";
 
 function App() {
 
+  useEffect(() => {
+    apitesting()
+  }, []);
+
+  function apitesting() {
+	fetchDataFromApi('/movie/popular').then((response)=>console.log(response))
+  }
+
   return (
     <>
-      <div className='App'>App</div>
+      <div className="App">App</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
